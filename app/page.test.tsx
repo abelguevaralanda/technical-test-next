a1095e75480377ba7b97b4a91ab7b13779f55ca1
Abel Guevara <abel.guevara@adsmurai.com>
2025-01-13 21:04:55 +0100

    Initial config

---
+++ app/page.test.tsx
@@ -0,0 +1,13 @@
+import Home from '@/app/page'
+import { render } from '@testing-library/react'
+import React from 'react'
+
+describe('Given a page component', () => {
+  describe('When its rendered', () => {
+    it('should render a text', () => {
+      const { getByText } = render(<Home />)
+
+      expect(getByText(/Get started by editing/)).toBeInTheDocument()
+    })
+  })
+})
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,54 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import stylistic from '@stylistic/eslint-plugin'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:@stylistic/recommended-extends',
+    'plugin:@typescript-eslint/stylistic',
+    'plugin:react/recommended',
+  ),
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'no-var': 'warn',
+      'implicit-arrow-linebreak': 'warn',
+      'array-bracket-spacing': 'warn',
+      'max-params': ['warn', { max: 3 }],
+      'max-depth': ['warn', { max: 3 }],
+      'no-lonely-if': 'warn',
+      'no-multiple-empty-lines': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/display-name': 'warn',
+      'react/jsx-props-no-spreading': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-param-reassign': 'off',
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      'import/order': 'off',
+      'object-curly-newline': ['error', { multiline: false, consistent: true }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
